5d097b10b5457cbab71f58a148b8cc98c9605f58
kilhem13 <guilhemlauro@gmail.com>
2019-10-12 14:45:41 +0200

    Add route of transfert page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Button } from '../node_modules/reactstrap'
+
+import {BrowserRouter, Route, Link } from 'react-router-dom';
+
 //import Login from './Account/Login.js';
 //import Signup from './Account/Signup.js'
 import Card from './Card/Card.js'
 import Wallet from './Wallet/Wallet.js'
+import Transfert from './Transfert/Transfert';
 
 
 
@@ -15,13 +20,18 @@ function App() {
   
   return (
     <div className="">
+      <BrowserRouter>
       <header className="">
        
       </header>
 
-      <Wallet  />
+      <Route exact path="/" component={() =>  <Wallet  />}/>
+  <Route exact path="/" component={() => <Link to="/transfert"><Button color="primary" title="Transfer" name="transfert" >Transfert</Button></Link> }/>
+      <Route path="/transfert" component={() => <Transfert/>}/>
+    </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
